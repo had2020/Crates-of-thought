44f040096287057250bad627571ba704241f9197
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-06-04 08:25:30 -0700

    Update main.rs

--- basic_hashing/src/main.rs
+++ basic_hashing/src/main.rs
@@ -5,18 +5,18 @@ fn main() {
     // init
     let mut map = HashMap::from([("Sam", 9), ("Mark", 2), ("Ben", 30)]);
 
-    //iterating
+    // iterating
     for key in map.keys() {
         println!("{key}");
     }
 
-    //inserting
+    // inserting
     map.insert("John", 45);
 
     println!("Who do you want to find?");
     let person: String = input();
 
-    //finding
+    // finding
     if let Some(key) = map.get(person.as_str()) {
         println!("key: {}", key);
     } else {
@@ -25,4 +25,7 @@ fn main() {
 
     // updating
     map.entry("John").or_insert(88);
+
+    // removing
+    map.remove("Sam");
 }
