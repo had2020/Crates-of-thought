ce054b53ce9c7dbc0fc1d4db93601a955ccefec2
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-07-07 16:03:31 -0700

    Update main.rs

--- custom_iterators/src/main.rs
+++ custom_iterators/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     let nums = vec![303, 404, 202];
     nums.iter().for_each(|i| println!("{i}"));
+
+    let nums1 = [1, 2, 3];
+    let mapped_nums1 = nums1.iter().map(|x| x * 2).collect::<Vec<i32>>();
+    println!("mapped_nums1: {:?}", mapped_nums1);
 }
