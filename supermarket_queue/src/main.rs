2ac3392a7ec7453fc4afcfbab1322240ebd9bf80
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-08-21 00:10:17 -0700

    updates

--- supermarket_queue/src/main.rs
+++ supermarket_queue/src/main.rs
@@ -9,47 +9,32 @@ The function should return an integer, the total time required.
 */
 
 fn queue_time(customers: &[u32], n: u32) -> u32 {
-    if !customers.is_empty() {
-        let mut waiting: Vec<u32> = customers.to_vec();
-        let mut iterations: u32 = 0;
-        let mut in_till: Vec<u32> = Vec::new();
-        let mut empty_tills: u32 = n;
-        loop {
-            if waiting.is_empty() && in_till.is_empty() {
-                break;
-            }
-            if !waiting.is_empty() {
-                if empty_tills == 0 {
-                    empty_tills = n;
-                }
-                for i in 0..empty_tills {
-                    if (waiting.len() as u32) > (i) {
-                        in_till.push(waiting[i as usize]);
-                        waiting.remove(i as usize);
-                    }
-                }
-                for i in 0..waiting.len() {
-                    waiting[i] += 1;
-                }
-            }
-            for i in 0..in_till.len() {
-                if i < in_till.len() {
-                    if in_till[i] == 1 {
-                        in_till.remove(i);
-                        empty_tills += 1;
+    let mut cus: Vec<u32> = customers.to_vec();
+    let mut iter = 0;
+    loop {
+        if !cus.is_empty() {
+            for i in 0..n {
+                if cus.len() > i as usize {
+                    if cus[i as usize] == 0 {
+                        cus.remove(i as usize);
                     } else {
-                        in_till[i] -= 1;
+                        cus[i as usize] -= 1;
                     }
                 }
             }
-            iterations += 1;
+        } else {
+            break;
         }
-        iterations
-    } else {
-        0
+        iter += 1;
     }
+    iter
 }
 
 fn main() {
-    println!("{}", queue_time(&[2, 2, 3, 3, 4, 4], 2));
+    println!("{}", queue_time(&[], 1)); // should be 0
+    println!("{}", queue_time(&[5], 1)); // 5
+    println!("{}", queue_time(&[2], 5)); // 2
+    println!("{}", queue_time(&[1, 2, 3, 4, 5], 1)); // 15
+    println!("{}", queue_time(&[1, 2, 3, 4, 5], 100)); // 5
+    println!("{}", queue_time(&[2, 2, 3, 3, 4, 4], 2)); // 9
 }
