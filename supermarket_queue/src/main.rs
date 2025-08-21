c7da4bf4f2f177d9378b755f666bf016a520a795
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-08-20 20:53:24 -0700

    Update main.rs

--- supermarket_queue/src/main.rs
+++ supermarket_queue/src/main.rs
@@ -1,3 +1,13 @@
+/*
+There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+
+input
+customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+n: a positive integer, the number of checkout tills.
+output
+The function should return an integer, the total time required.
+*/
+
 fn queue_time(customers: &[u32], n: u32) -> u32 {
     if !customers.is_empty() {
         let mut waiting: Vec<u32> = customers.to_vec();
