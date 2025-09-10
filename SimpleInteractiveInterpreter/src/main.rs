5614329d14335734c913a8758d623302ac86fe8b
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-09-10 10:14:32 -0700

    updates

---
+++ SimpleInteractiveInterpreter/src/main.rs
@@ -0,0 +1,11 @@
+struct Interpreter {}
+
+impl Interpreter {
+    fn new() -> Interpreter {
+        unimplemented!()
+    }
+
+    fn input(&mut self, input: &str) -> Result<Option<f32>, String> {
+        unimplemented!()
+    }
+}
