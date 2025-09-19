038f40bea70af897a96c422800194c0f75c1cab0
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-09-19 12:29:07 -0700

    updates

---
+++ CalPI/src/main.rs
@@ -0,0 +1,15 @@
+use std::time::{Duration, Instant};
+
+fn pi_leibniz(iterations: u64) -> f64 {
+    let mut pi = 0.0;
+    for k in 0..iterations {
+        let term = (-1.0f64).powi(k as i32) / (2 * k + 1) as f64;
+        pi += term;
+    }
+    4.0 * pi
+}
+
+fn main() {
+    let start = Instant::now();
+    println!("time:{:?}, pi:{}", start.elapsed(), pi_leibniz(1_000_000));
+}
