b8b0319149f1587fcee7fdc585432237d309ce90
Hadrian Lazic <hadrian.lazic@gmail.com>
2025-11-21 12:52:28 -0800

    updates

--- FixThePipes2IsItLeaking/src/main.rs
+++ FixThePipes2IsItLeaking/src/main.rs
@@ -1,20 +1,32 @@
+pub fn source_check(c:char) -> (bool, char) {
+    return match c {
+        '' => (true, '')
+        - => (false, '')
+    }
+}
 fn check_pipe(pipe_map: &[&str]) -> bool {
     let mut rt: Vec<Vec<char>> = Vec::new();
     for i in 0..pipe_map.len() {
         rt.push(pipe_map[i].chars().collect());
     }
-    let mut ws_poss: Vec<(usize, usize)> = Vec::new();
-    for i in 0..rt.len() { // ws scanning
-        for j in 0..rt[i].len() {
-            if rt[i][j] == '+' {
-                ws_poss.push((i,j));
-            }
+    let mut ws_poss: Vec<(usize, usize, char)> = Vec::new();
+    // ws edge scanning
+    for i in 0..rt.len() { // TL and TR
+        if rt[0][i] == '┗' || rt[0][i] == '┓' || {
+            ws_poss.push((0,i));
         }
+        if rt[i][0] == '' {
+            ws_poss.push((i,0));
+        }
+        match rt[0]
     }
+
     let mut r: bool = true;
     for ws in ws_poss {
         let n_cels = [rt[ws.0][(ws.1)+1], rt[ws.0][(ws.1).saturating_sub(1)], rt[(ws.0)+1][ws.1], rt[(ws.0).saturating_sub(1)][ws.1]];
-        for _ in n_cels
+        for c in n_cels {
+            todo!("matching pipes, and recersive!")
+        }
     }
     return r
 }
