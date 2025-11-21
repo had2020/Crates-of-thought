c0aa81b5cd553a5f0a11a03817ed9df8ef58dd5e
Hadrian Lazic <hadrian.lazic@gmail.com>
2025-11-21 10:50:20 -0800

    new code wars problem

---
+++ FixThePipes2IsItLeaking/src/main.rs
@@ -0,0 +1,9 @@
+fn check_pipe(pipe_map: &[&str]) -> bool {
+    let mut rt: Vec<Vec<char>> = Vec::new();
+    for i in 0..pipe_map.len() {
+        rt.push(pipe_map[i].chars().collect());
+    }
+    let mut wss:  
+    
+    return false
+}
