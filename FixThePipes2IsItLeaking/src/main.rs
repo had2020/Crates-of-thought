704eb4631c8a27f4e16bce07ac613d61a6265157
Hadrian Lazic <hadrian.lazic@gmail.com>
2025-11-21 11:00:53 -0800

    Update main.rs

--- FixThePipes2IsItLeaking/src/main.rs
+++ FixThePipes2IsItLeaking/src/main.rs
@@ -3,7 +3,14 @@ fn check_pipe(pipe_map: &[&str]) -> bool {
     for i in 0..pipe_map.len() {
         rt.push(pipe_map[i].chars().collect());
     }
-    let mut wss:  
+    let mut ws_poss: Vec<(usize, usize)> = Vec::new();
+    for i in 0..rt.len() { // ws scanning
+        for j in 0..rt[i].len() {
+            if rt[i][j] == '+' {
+                ws_poss.push((i,j));
+            }
+        }
+    }
     
     return false
 }
