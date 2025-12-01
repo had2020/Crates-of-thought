5eaebc8a101e7edfe5b8d42abd96fccf7a567c38
Hadrian Lazic <hadrian.lazic@gmail.com>
2025-12-01 14:42:44 -0800

    Update main.rs

--- FixThePipes2IsItLeaking/src/main.rs
+++ FixThePipes2IsItLeaking/src/main.rs
@@ -1,14 +1,25 @@
-enum PipDir {TR,TL,BR,BL,T,B,L,R,C}
-fn is_source(c:char, x: usize, y: usize, m_x: usize, m_y: usize) -> (bool, char) {
-    let pipdir = match (x,y) {
-        (0,0) => PipDir::TL,
-        (m_x,0) => PipDir::TR, 
-        (0,m_y) => PipDir::BL,
-        (m_x,m_y) => PipDir::BR,
-        (0,_) => {PipDir::T},
-        (_,m_y) => PipDir::B,
-        (_,0) => PipDir::L,
-        (_,m_x) => PipDir::R,
+use std::collections::HashMap;
+enum PipDir {
+    TR,
+    TL,
+    BR,
+    BL,
+    T,
+    B,
+    L,
+    R,
+    C,
+}
+fn is_source(c: char, x: usize, y: usize, mx: usize, my: usize) -> (bool, char) {
+    let pipdir = match (x, y) {
+        (0, 0) => PipDir::TL,
+        (mx, 0) => PipDir::TR,
+        (0, my) => PipDir::BL,
+        (mx, my) => PipDir::BR,
+        (0, _) => PipDir::T,
+        (_, my) => PipDir::B,
+        (_, 0) => PipDir::L,
+        (_, mx) => PipDir::R,
         _ => PipDir::C,
     };
     let r = match pipdir {
@@ -56,8 +67,9 @@ fn is_source(c:char, x: usize, y: usize, m_x: usize, m_y: usize) -> (bool, char)
             '╋' => (true, c),
             _ => (true, c),
         },
-        _ => (false, c)
-    }; return r
+        _ => (false, c),
+    };
+    return r;
 }
 fn check_pipe(pipe_map: &[&str]) -> bool {
     let mut rt: Vec<Vec<char>> = Vec::new();
@@ -65,44 +77,61 @@ fn check_pipe(pipe_map: &[&str]) -> bool {
         rt.push(pipe_map[i].chars().collect());
     } // ws edge scanning
     let mut ws_poss: Vec<(usize, usize, char)> = Vec::new();
-    for i in 0..rt.len() { // TL and TR
-        let n0 = is_source(rt[0][i], 0, i, rt[i].len()-1, rt.len()-1);
+    for i in 0..rt.len() {
+        // TL and TR
+        let n0 = is_source(rt[0][i], 0, i, rt[i].len() - 1, rt.len() - 1);
         if n0.0 {
-            ws_poss.push((0,i, n0.1));
+            ws_poss.push((0, i, n0.1));
         }
-        let n1 = is_source(rt[i][0], 0, i, rt[i].len()-1, rt.len()-1);
+        let n1 = is_source(rt[i][0], 0, i, rt[i].len() - 1, rt.len() - 1);
         if n1.0 {
-            ws_poss.push((i,0, n1.1));
+            ws_poss.push((i, 0, n1.1));
         }
     }
     let mut r: bool = true;
     for ws in ws_poss {
-        let t_cel = ((ws.0), (((ws.1)+1)), rt[ws.0][(ws.1)+1]);
-        let b_cel = ((ws.0), ((ws.1).saturating_sub(1)), rt[ws.0][(ws.1).saturating_sub(1)]);
-        let l_cel = (((ws.0)+1), (ws.1), rt[(ws.0)+1][ws.1]);
-        let r_cel = (((ws.0).saturating_sub(1)), (ws.1), rt[(ws.0).saturating_sub(1)][ws.1]);
-        let mut vists: Vec<Vec<bool>> = Vec::new();
-        let mut tasks: Vec<(usize,usize, char)> = vec!(t_cel, b_cel, l_cel, r_cel);
-        loop {
-            for i in 0..tasks.len() {
-                match rt[tasks[i].0][tasks[i].1] {
-                    '┗' => {},
-                    '┓' => {},
-                    '┏' => {},
-                    '┛' => {},
-                    '━' => {},
-                    '┃' => {},
-                    '┣' => {},
-                    '┫' => {},
-                    '┳' => {},
-                    '┻' => {},
-                    '╋' => {},
-                    _ => {},
+        let mut t_cel: (usize, usize, char) = (ws.0, ws.1, rt[ws.0][(ws.1) + 1]);
+        if pipe_map[0].len() > (ws.1) + 1 {
+            t_cel = (ws.0, ws.1 + 1, rt[ws.0][ws.1 + 1]);
+        }
+        let b_cel: (usize, usize, char) = (
+            (ws.0),
+            ((ws.1).saturating_sub(1)),
+            rt[ws.0][(ws.1).saturating_sub(1)],
+        );
+        let mut l_cel: (usize, usize, char) = (ws.0 + 1, ws.1, rt[(ws.0) + 1][ws.1]);
+        if pipe_map.len() > (ws.0) + 1 {
+            l_cel = (((ws.0) + 1), (ws.1), rt[(ws.0) + 1][ws.1]);
+        }
+        let r_cel: (usize, usize, char) = (
+            ((ws.0).saturating_sub(1)),
+            (ws.1),
+            rt[(ws.0).saturating_sub(1)][ws.1],
+        );
+        let mut vists: HashMap<(usize, usize), bool> = HashMap::new();
+        let mut tasks: Vec<(usize, usize, char)> = vec![t_cel, b_cel, l_cel, r_cel];
+        while tasks.len() > 0 {
+            if vists.contains_key(&(tasks[0].0, tasks[0].1)) {
+                match rt[tasks[0].0][tasks[0].1] {
+                    '┗' => {}
+                    '┓' => {}
+                    '┏' => {}
+                    '┛' => {}
+                    '━' => {}
+                    '┃' => {}
+                    '┣' => {}
+                    '┫' => {}
+                    '┳' => {}
+                    '┻' => {}
+                    '╋' => {}
+                    _ => (),
                 }
-                vists.push(tasks[i].clone());
-                tasks.remove(i);
+                vists.insert((tasks[0].0, tasks[0].1), true);
+                tasks.remove(0);
+            } else {
+                tasks.remove(0);
             }
         }
     }
-    return r
+    return r;
 }
