e23c7bfd1b2be905c158b75d61a71a1a2bb54bc0
had2020 <hadrian.lazic@gmail.com>
2026-08-01 10:50:18 -0700

    upd

---
+++ reversed_strings_in_c/main.c
@@ -0,0 +1,15 @@
+#include <string.h>
+char *strrev (char *string)
+{
+  size_t len = strlen(string);
+  size_t from_back = len - 1;
+  size_t half_len = len / 2;
+  
+  for (int i = 0; i < half_len; i++) {
+    char copy = string[from_back];
+    string[from_back] = string[i];
+    string[i] = copy;
+    from_back--;
+  }
+  return string;
+}
