079e7faa88bbe772b17cd23b8bc53828cf2ebed8
had2020 <hadrian.lazic@gmail.com>
2026-08-13 17:45:36 -0700

    codewars solution Basic Mathematical Operations

---
+++ basic_mathematical_operations/main.cpp
@@ -0,0 +1,16 @@
+int basicOp(char op, int val1, int val2) {
+  switch (op) {
+  case '+': {
+    return val1 + val2;
+  }
+  case '-': {
+    return val1 - val2;
+  }
+  case '*': {
+    return val1 * val2;
+  }
+  case '/': {
+    return val1 / val2;
+  }
+  }
+}
