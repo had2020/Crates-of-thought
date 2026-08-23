3261c0e07aa08a997a93fbe42da4b5de5977f284
had2020 <hadrian.lazic@gmail.com>
2026-08-23 10:59:53 -0700

    upd

---
+++ reverse_bits/reverse_bits.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdio.h>
+
+void printBinary(int num) {
+  for (int i = sizeof(int) * 8 - 1; i >= 0; i--) {
+    printf("%d", (num >> i) & 1);
+  }
+  printf("\n");
+}
+
+int reverseBits(int n) {
+  int cn = 0;
+  const int nlen = sizeof(n) * 8;
+  for (int i = nlen; i < 0; i--) {
+    cn &= (n >> i) << i;
+  }
+  return cn;
+}
+
+int main() {
+  printBinary(reverseBits(0b01011));
+  return 0;
+}
